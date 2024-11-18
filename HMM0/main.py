a1d5c6a73cbacfeb94ca8692efd8eb3492e7903e
Hackerman25 <marcus.loberg@outlook.com>
2024-11-18 12:23:44 +0100

    changed into main and matrixmult funcs

--- HMM0/main.py
+++ HMM0/main.py
@@ -16,51 +16,68 @@ def read_matrices(data):
 
     return matrices
 
-# Example input data
-data = """4 4 0.2 0.5 0.3 0.0 0.1 0.4 0.4 0.1 0.2 0.0 0.4 0.4 0.2 0.3 0.0 0.5
-4 3 1.0 0.0 0.0 0.0 1.0 0.0 0.0 0.0 1.0 0.2 0.6 0.2
-1 4 0.0 0.0 0.0 1.0"""
-
-# Read matrices
-matrices = read_matrices(data)
-
-# Print matrices
-for matrix in matrices:
-    for row in matrix:
-        print(row)
-    print()
 
 
-# calc output
-
-ProbObsEachObs= []
 
 
-i = 0
 
-for i in range(len(matrices[2][0])):
-    tmp = 0
-    for e in range(len(matrices[0])):
-        #print(matrices[0][i][e] , matrices[2][0][i])
-        tmp = tmp + matrices[0][e][i] * matrices[2][0][e]
-    ProbObsEachObs.append(tmp)
+def matrixmult(a,b):
+    output = [[0] * len(b[0])]*len(a)
+    print(output)
 
-#print(ProbObsEachObs)
+    for i in range(len(a)):
+
+        for j in range(len(b[0])):
+
+            for k in range(len(b)):
+
+                output[i][j] += a[i][k] * b[k][j]
+
+    for i in range(len(output)):
+        for j in range(len(output[0])):
+            output[i][j] = round(output[i][j],2)
+
+    return output
 
 
-ans= []
-for i in range(len(matrices[1][0])):
-    tmp = 0
-    #print(len(matrices[1]),"dkdkd")
-    for e in range(len(matrices[1])):
+def main():
+    # Example input data
+    data = """4 4 0.2 0.5 0.3 0.0 0.1 0.4 0.4 0.1 0.2 0.0 0.4 0.4 0.2 0.3 0.0 0.5
+    4 3 1.0 0.0 0.0 0.0 1.0 0.0 0.0 0.0 1.0 0.2 0.6 0.2
+    1 4 0.0 0.0 0.0 1.0"""
 
-        tmp = tmp + matrices[1][e][i] * ProbObsEachObs[e]
-        #tmp = tmp + matrices[0][e][i] * matrices[2][0][e]
-    ans.append(round(tmp,2))
+    # Read matrices
+    matrices = read_matrices(data)
 
-print(ans)
-#transition matrix = matrix of prob to go from state to other state
-#emission matrix   = matrix of prob to get to observed based on state
-# initial state probability distribution = initial states prob
+    # Print matrices
+    for matrix in matrices:
+        for row in matrix:
+            print(row)
+        print()
 
-#output =
+    # calc output
+
+
+    """
+    for i in range(len(matrices[1][0])):
+        tmp = 0
+        #print(len(matrices[1]),"dkdkd")
+        for e in range(len(matrices[1])):
+    
+            tmp = tmp + matrices[1][e][i] * ProbObsEachObs[e]
+            #tmp = tmp + matrices[0][e][i] * matrices[2][0][e]
+        ans.append(round(tmp,2))
+    """
+    ans = matrixmult(matrices[2], matrices[0])
+    print(ans)
+
+    an2 = matrixmult(ans, matrices[1])
+
+    print(an2, "ans")
+    #transition matrix = matrix of prob to go from state to other state
+    #emission matrix   = matrix of prob to get to observed based on state
+    # initial state probability distribution = initial states prob
+
+
+if __name__ == "__main__":
+    main()
