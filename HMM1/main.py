425ddb4dc274c49d0a3d54f3c551db663e8cc258
Hackerman25 <marcus.loberg@outlook.com>
2024-12-04 17:12:40 +0100

    HMM1 progress

--- HMM1/main.py
+++ HMM1/main.py
@@ -15,7 +15,10 @@ def read_data(data):
         matrix = [values[j:j + cols] for j in range(0, len(values), cols)]
         matrices.append(matrix)
 
-    matrices.append(lines[-1][2:])
+
+
+    last_line_values = list(map(int, lines[-1].split()))[2:]
+    matrices.append(last_line_values)
     return matrices
 
 
@@ -29,7 +32,7 @@ def main():
     data += "\n" + input()
     data += "\n" + input()
     data += "\n" + input()
-    print(data)
+
     #print(data)
 
 
@@ -45,14 +48,77 @@ def main():
     """
     # calc output
 
+    trans_matrix = handledfile[0] #A
+    obs_matrix = handledfile[1]   #B  emission matrix
+    init_prob = handledfile[2]    #Pi
+    sequence_of_emissions = handledfile[3] # output
 
-    sequence_of_emissions = handledfile[3]
-    init_prob = handledfile[2]
-    obs_matrix = handledfile[1]
-    trans_matrix = handledfile[0]
 
+    print(trans_matrix)
+    print(obs_matrix)
+    print(init_prob)
     print(sequence_of_emissions)
+    #print(obs_matrix)
+    #print(init_prob)
 
+
+    def forward_alg(trans_matrix,obs_matrix,init_prob,sequence_of_emissions):
+
+        observation_0 = sequence_of_emissions[0]
+
+
+
+
+        a_vector = [None]*len(sequence_of_emissions)
+        a_0 = [None]*len(trans_matrix)
+
+
+        for i in range(0,len(trans_matrix)): #correct
+
+            a_0[i] = int(init_prob[0][i]) * obs_matrix[i][int(observation_0)]
+        a_vector[0] = a_0
+
+
+
+
+
+        for t in range(1,len(sequence_of_emissions)):
+
+            observation_t = sequence_of_emissions[t]
+
+
+            a_t_prev = a_vector[t-1]
+
+            a_t = [None] * len(trans_matrix)
+            for i in range(0,len(trans_matrix)):
+                sum = 0
+                for j in range(0,len(trans_matrix)):
+                    sum += a_t_prev[j] * trans_matrix[j][i]
+
+                a_t[i] = sum * obs_matrix[i][observation_t]
+
+            a_vector[t] = a_t
+
+
+        """
+        alpha = [[init_prob[0][i] * obs_matrix[i][sequence_of_emissions[0]] for i in range(len(init_prob[0]))]]
+        for t in range(1, len(sequence_of_emissions)):
+            alpha.append([sum([alpha[t - 1][j] * trans_matrix[j][i]
+                for j in range(len(trans_matrix))]) * obs_matrix[i][sequence_of_emissions[t]] for i in range(len(trans_matrix))])
+        """
+
+        return a_vector
+
+
+
+
+    a_vector = forward_alg(trans_matrix, obs_matrix, init_prob, sequence_of_emissions)
+
+    output = sum(a_vector[-1])
+
+    #output2 = sum(alpha[-1])
+    print(output,"output")
 
 if __name__ == "__main__":
+
     main()
